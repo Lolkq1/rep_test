2c2b6170aed5a7564e5b50686cb40a171a21cb0d
Aluno IFAL <alunoifal@MACEIO.IFAL>
2025-11-12 08:41:03 -0300

    tooois

--- bgl.js
+++ bgl.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mysql2 = require('mysql2')
+require('dotenv').config()
+console.log(process.env.PASS)
+const con = mysql2.createConnection({
+    user: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'testee'
+})
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.use(express.static(path.join(__dirname, 'public')))
+
+app.post('/search', (req, res) => {
+    let v=''
+    req.on('data' , (chunk) => {
+        v+=chunk
+    })
+    req.on('end', () => {
+        let v2 = JSON.parse(v)
+        console.log(v2)
+        con.query('SELECT * FROM livros WHERE nome=?', [v2.nome], (err, data) => {
+            if (err) {
+                console.log('error')
+                res.status(500).send('internal server error')
+            } else {
+                console.log(data)
+                console.log(JSON.stringify({
+                    livros:data
+                }))
+                res.send(JSON.stringify({
+                    livros:data
+                }))
+            }
+        })
+    })
+})
 
 app.listen(8080, () => {
     console.log('sv rodando em 8080')
 })
+
--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
-console.log('testando')
+const showcase = document.querySelector(".livros")
+const btn = document.querySelector("#btn")
+const t = document.querySelector("#nome")
+btn.addEventListener("click", () => {
+    console.log(t.value)
+    fetch("/search", {
+        method:'POST',
+        body: JSON.stringify({
+            nome:t.value
+        })
+    }).then(res => JSON.parse(res)).then(obj => {
+        if (obj.livros.length === 0) {
+            alert('Não foram encontrados livros com esse nome.')
+        } else {
+            for (x of obj.livros) {
+                let d = document.createElement('div')
+                d.style.width='200px'
+                d.style.height='200px'
+                showcase.appendChild(d)
+                let p = document.createElement('p')
+                p.textContent='Nome: '+x.nome
+                let p2 = document.createElement('p')
+                p.textContent='Autor: '+x.autor
+                let p3 = document.createElement('p')
+                p.textContent='Ano de lançamento: '+x.ano_de_lancamento
+                d.appendChild(p)
+                d.appendChild(p2)
+                d.appendChild(p3)
+            }
+        }
+    })
+})
