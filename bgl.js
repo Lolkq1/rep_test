0298510c182b8085532dbdd52beb97bb6d65b871
Aluno IFAL <alunoifal@MACEIO.IFAL>
2025-11-12 08:00:54 -0300

    noooisss

--- bgl.js
+++ bgl.js
@@ -4,3 +4,9 @@ const path = require('path')
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(8080, () => {
+    console.log('sv rodando em 8080')
+})
